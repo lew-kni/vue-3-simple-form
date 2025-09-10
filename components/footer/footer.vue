31a1d22ddc95dee9a6cf6737da48884d4603be61
Lew <lew.kni@gmail.com>
2025-09-10 15:36:31 +0100

    Updates

--- components/footer/footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div class="mt-6">
-		<footer class="bg-gray-800 text-white p-4 text-center">
-			&copy; 2025 Insure & Protect.
-		</footer>
-	</div>
-</template>
-<script setup lang="ts"></script>
-<style scoped lang="scss"></style>
--- components/form/FormTitle.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<h2
-		class="text-black text-3xl pb-2 font-semibold mb-4 w-full border-b-2 border-black"
-	>
-		<slot></slot>
-	</h2>
-</template>
-<script setup lang="ts"></script>
-<style scoped lang="scss"></style>
